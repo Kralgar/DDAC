722be469c00a3fc8a3cb1ce06fc14c3e35c7165a
Kralgar <Kralgar@hotmail.com>
2018-04-10 14:10:06 +0800

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbServerName = 'localhost';
-$dbUserName = 'root';
-$dbPassword = '';
+$dbServerName = 'maerskddactp039167-mysqldbserver.mysql.database.azure.com';
+$dbUserName = 'mysqldbuser@maerskddactp039167-mysqldbserver';
+$dbPassword = 'pass@123';
 $dbName = 'cms_database';
 
 $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
