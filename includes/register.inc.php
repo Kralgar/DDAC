3b8163a896c9e68ec7f05e7c0598b334497cfc22
Kralgar <Kralgar@hotmail.com>
2018-04-10 16:35:35 +0800

    Update register.inc.php

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -28,30 +28,6 @@ else {
 				header('Location: ..\register.php?register=email');
 			}
 			else {
-				/*
-				$sql = 'SELECT * FROM users WHERE u_username="'.$username.'";';
-				$result = mysqli_query($conn, $sql);
-				$resultCheck = mysqli_num_rows($result);
-				if ($resultCheck > 0) {
-					header('Location: ..\register.php?register=usertaken');
-				}
-				else {
-					//hashing the password
-					$hashedPwd = password_hash($password, PASSWORD_DEFAULT); 
-					//insert the user into the database
-					$sql = 'INSERT INTO users (u_first_name, u_last_name, u_email, u_username, u_password) 
-						VALUES (?, ?, ?, ?, ?);';
-					$stmt = mysqli_stmt_init($conn);	
-					if (!mysqli_stmt_prepare($stmt, $sql)) {
-						header('Location: ..\register.php?register=sqlerror');
-					}
-					else {
-						mysqli_stmt_bind_param($stmt, 'sssss', $first, $last, $email, $username, $hashedPwd);
-						mysqli_stmt_execute($stmt);
-						header('Location: ..\register.php?register=success');
-					}
-				}*/
-				
 				$sql = 'SELECT * FROM users WHERE u_username=?;';
 				$stmt = mysqli_stmt_init($conn);
 				
